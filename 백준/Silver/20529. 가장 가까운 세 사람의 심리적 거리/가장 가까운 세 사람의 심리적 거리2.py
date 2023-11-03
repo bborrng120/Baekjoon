17d06a16e130c397304249604355e80135eaffa8
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-03 23:40:58 +0900

    Create 가장 가까운 세 사람의 심리적 거리2.py

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리2.py
@@ -0,0 +1,45 @@
+from itertools import combinations
+import sys
+
+n = int(sys.stdin.readline())
+
+def check(x):
+    count = 0
+    if x[0][0] != x[1][0]:
+        count += 1
+    if x[0][1] != x[1][1]:
+        count += 1
+    if x[0][2] != x[1][2]:
+        count += 1
+    if x[0][3] != x[1][3]:
+        count += 1
+    return count
+    
+for _ in range(n):
+    a = int(sys.stdin.readline())
+    m = sys.stdin.readline().split()
+    mbti = {}
+    mins = 100001
+    for k in m:
+        mbti[k] = 0
+    
+    if a > 32:
+        print(0)
+        continue
+    
+    else:
+        for alpha in m:
+            mbti[alpha] += 1
+            if mbti[alpha] == 3:
+                print(0)
+                break
+            
+        else:
+            for i in list(combinations(m,3)):
+                sum, count = 0, 0
+                for j in list(combinations(i,2)):
+                    count = check(j)
+                    sum += count
+                if mins > sum:
+                    mins = sum
+            print(mins)
