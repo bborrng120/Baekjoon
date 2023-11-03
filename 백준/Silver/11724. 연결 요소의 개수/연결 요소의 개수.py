d63e2d3a1f7dfbd8e23d2e23b25139cc12bcea14
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-03 22:30:12 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 564 ms, Memory: 38416 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+n, m= map(int, sys.stdin.readline().split())
+count = 0
+
+matrix = [[0]*(n+1) for i in range(n+1)]
+v_dfs = [0]*(n+1)
+
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    matrix[a][b] = matrix[b][a] = 1
+                
+def dfs(v):
+    v_dfs[v] = 1
+    for i in range(1, n+1):
+        if v_dfs[i] == 0 and matrix[v][i] == 1:
+            dfs(i)
+
+for i in range(1, len(v_dfs)):
+    if v_dfs[i] != 1:
+        count+=1
+        dfs(i)
+        
+print(count)
