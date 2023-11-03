a96ad7409c256abc1b423d8230513091dc9d7bf3
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-03 23:26:16 +0900

    Create N과 M （1）2.py

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）2.py
@@ -0,0 +1,17 @@
+import sys
+
+n, m = map(int,sys.stdin.readline().split())
+s = []
+
+def back():
+    if len(s) == m:
+        print(*s)
+        return
+        
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            back()
+            s.pop()
+            
+back()
