c0ea0a302caac5da71395063589afaee1f0e8386
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-17 11:36:56 +0900

    [Gold V] Title: 동전 2, Time: 376 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,21 +1,17 @@
-import math
 import sys
 
 n, k = map(int,sys.stdin.readline().split())
 coin = []
-d = [math.inf]*(k+1)
+d = [int(1e9)]*(k+1)
 d[0] = 0
 for _ in range(n):
     coin.append(int(sys.stdin.readline()))
     
 for i in coin:
     for j in range(i,k+1):
-        if d[j-i]==math.inf:
-            continue
-        else:
-            d[j] = min(d[j-i]+1,d[j])
+        d[j] = min(d[j-i]+1,d[j])
 
-if d[k]==math.inf:
+if d[k]==int(1e9):
     print(-1)
 else:
     print(d[k])
