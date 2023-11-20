49628f70ade597ef8b3be54bfce7299be28e2077
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-20 09:56:01 +0900

    [Silver III] Title: 빈도 정렬, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,11 +1,19 @@
-from collections import Counter
 import sys
 
 n, c = map(int,sys.stdin.readline().split())
 my_list = list(map(int,sys.stdin.readline().split()))
 
-count = Counter(my_list)
-count = count.most_common(n)
-for i,j in count:
-    for _ in range(j):
-        print(i, end=" ")
+dic = {}
+idx = 0
+for i in my_list:
+    if i in dic:
+        dic[i][0]+=1
+    else:
+        dic[i] = [1,idx]
+        idx += 1
+        
+ans = [[i,j] for i,j in dic.items()]
+ans.sort(key=lambda x:(-x[1][0],x[1][1]))
+for i,j in ans:
+    for _ in range(j[0]):
+        print(i,end=" ")
