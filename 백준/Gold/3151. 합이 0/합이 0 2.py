032b350808652752a89e4f4c53258c0be31b8d4d
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-11-30 16:50:28 +0900

    Create 합이 0 2.py

---
+++ 백준/Gold/3151. 합이 0/합이 0 2.py
@@ -0,0 +1,26 @@
+import bisect
+import sys
+
+n = int(sys.stdin.readline())
+my_list = list(map(int,sys.stdin.readline().split()))
+my_list.sort()
+count = 0
+
+for i in range(n-2):
+    l,r = i+1, n-1
+        
+    while l<r:
+        sums = my_list[l]+my_list[r]+my_list[i]
+        
+        if sums>0:
+            r-=1
+        else:
+            if sums==0:
+                if my_list[l]==my_list[r]:
+                    count += r-l
+                else:
+                    s = bisect.bisect_left(my_list,my_list[r])
+                    count += r-s+1
+            l+=1
+            
+print(count)
