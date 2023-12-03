98b8c6a6b9f3a40436847c30adac2f78e65a8ea3
hufsicesohn <101964793+hufsicesohn@users.noreply.github.com>
2023-12-03 22:17:20 +0900

    [Silver III] Title: 타노스, Time: 64 ms, Memory: 34052 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -3,5 +3,24 @@ import sys
 
 s = sys.stdin.readline().strip()
 count = Counter(s)
+o_list,z_list = [],[]
+ans = ''
 
-print('0'*(count['0']//2) + '1'*(count['1']//2))
+for i in range(len(s)):
+    if s[i]=='0':
+        z_list.append((i,'0'))
+    else:
+        o_list.append((i,'1'))
+        
+for _ in range(count['0']//2):
+    z_list.pop()
+    
+for _ in range(count['1']//2):
+    o_list.pop(0)
+    
+ans_list = sorted(z_list+o_list,key=lambda x:x[0])
+
+for i in ans_list:
+    ans+=i[1]
+    
+print(ans)
