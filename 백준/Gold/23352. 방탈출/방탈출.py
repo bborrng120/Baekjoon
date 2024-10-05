760c70fc2b66f6a67dde4916be1e33190e224561
hufsicesohn <101964793+bborrng120@users.noreply.github.com>
2024-10-05 22:44:06 +0900

    [Gold V] Title: 방탈출, Time: 4484 ms, Memory: 152600 KB -BaekjoonHub

--- 백준/Gold/23352. 방탈출/방탈출.py
+++ 백준/Gold/23352. 방탈출/방탈출.py
@@ -3,54 +3,39 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 my_list = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-max_length = 0
-ans = 0
-count = 0
+ans_list = []
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def bfs(x, y, visit):
+def bfs(x, y):
     first = my_list[x][y]
-    q = deque([(x,y,1,first)])
+    q = deque([(x,y,1)])
+    
+    visit = [[0]*m for _ in range(n)]
     visit[x][y] = 1
-    maxs_l = 0
-    maxs_c = 0
     
     while q:
-        x, y, l, c = q.popleft()
+        x, y, l = q.popleft()
+        check = False
         
-        if maxs_l == l:
-            maxs_c = max(c, maxs_c)
-        elif maxs_l < l:
-            maxs_l = l
-            maxs_c = c
-            
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
             if 0<=nx<n and 0<=ny<m and my_list[nx][ny]!=0:
                 if visit[nx][ny]==0:
-                    q.append((nx,ny,l+1,first+my_list[nx][ny]))
+                    check = True
+                    q.append((nx,ny,l+1))
                     visit[nx][ny] = 1
-                    
-    return maxs_c, maxs_l
+        
+        if not check:
+            ans_list.append((l, first+my_list[x][y]))
 
 for i in range(n):
     for j in range(m):
-        if not my_list[i][j]:
-            count += 1
-            continue
-        visit = [[0]*m for _ in range(n)]
-        res, length = bfs(i, j, visit)
-        if max_length == length:
-            ans = max(res, ans)
-        elif max_length < length:
-            max_length = length
-            ans = res
+        if my_list[i][j]:
+            bfs(i, j)
             
-if count == n*m:
-    print(0)
-else:
-    print(ans)
+ans_list.sort(key=lambda x:(x[0], x[1]))
+print(ans_list[-1][1])
