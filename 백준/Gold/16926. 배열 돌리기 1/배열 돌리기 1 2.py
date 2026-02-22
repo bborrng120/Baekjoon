c7b0bfb8875c72e2e5908634300aabc7bf4b463e
hufsicesohn <101964793+bborrng120@users.noreply.github.com>
2026-02-22 22:32:01 +0900

    Create 배열 돌리기 1 2.py

---
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1 2.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+n, m, r = map(int, sys.stdin.readline().split())
+my_list = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+for k in range(min(n,m)//2):
+    q = deque()
+    
+    for j in range(k, m-k-1):
+        q.append(my_list[k][j])
+    for i in range(k, n-k-1):
+        q.append(my_list[i][m-k-1])
+    for j in range(m-k-1, k, -1):
+        q.append(my_list[n-k-1][j])
+    for i in range(n-k-1, k, -1):
+        q.append(my_list[i][k])
+        
+    q.rotate(-r)
+    
+    for j in range(k, m-k-1):
+        my_list[k][j] = q.popleft()
+    for i in range(k, n-k-1):
+        my_list[i][m-k-1] = q.popleft()
+    for j in range(m-k-1, k, -1):
+        my_list[n-k-1][j] = q.popleft()
+    for i in range(n-k-1, k, -1):
+        my_list[i][k] = q.popleft()
+        
+for i in my_list:
+    print(*(i))
