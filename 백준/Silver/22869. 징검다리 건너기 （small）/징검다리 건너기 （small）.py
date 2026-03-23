a90d098acd5f293530dcd7d86c4e35ac622328b7
hufsicesohn <101964793+bborrng120@users.noreply.github.com>
2026-03-23 22:21:27 +0900

    [Silver I] Title: 징검다리 건너기 (small), Time: 3756 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.py
@@ -0,0 +1,18 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+my_list = list(map(int, sys.stdin.readline().split()))
+d = [0]*n
+d[0] = 1
+
+for i in range(n-1):
+    if d[i]:
+        for j in range(i+1, n):
+            if d[j] == 0 and (j-i) * (1 + abs(my_list[i]-my_list[j])) <= k:
+                d[j] = 1
+                
+if d[n-1]:
+    print("YES")
+else:
+    print("NO")
+            
